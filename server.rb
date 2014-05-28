0b49a90886def72df94c2dd97543ab7531755745
Helen Hood <helen.c.hood@gmail.com>
2014-05-28 09:55:57 -0400

    Actor show action retrieves actors' movies

--- server.rb
+++ server.rb
@@ -32,11 +32,26 @@ end
 get '/actors/:id' do
   actor_id = params[:id]
 
+  query = %Q{
+    SELECT actors.name AS actor, movies.title AS movie_title, movies.id AS movie_id
+    FROM actors
+    JOIN cast_members ON actors.id = cast_members.actor_id
+    JOIN movies ON movies.id = cast_members.movie_id
+    WHERE actors.id = $1;
+  }
+
   results = db_connection do |conn|
-    conn.exec_params('SELECT * FROM actors WHERE id = $1', [actor_id])
+    results = conn.exec_params(query, [actor_id])
+    results.to_a
   end
 
-  @actor = results.to_a[0]
+  @actor_name = results[0]['actor']
+
+  @movies = []
+
+  results.each do |result|
+    @movies << { title: result['movie_title'], id: result['movie_id'] }
+  end
 
   erb :'actors/show'
 end
